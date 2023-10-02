bff7e232d4965b67078dbd3bd674f212362015f1
Sachin Yadav <sachin.y4dav@gmail.com>
2023-10-02 18:02:01 -0400

    ADD: user service test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import mongoConfig from './config/mongoConfig';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./modules/users/users.module";
+import { AuthModule } from "./modules/auth/auth.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import mongoConfig from "./config/mongoConfig";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.dev.env',
+      envFilePath: ".dev.env",
       cache: true,
       isGlobal: true,
       load: [mongoConfig],
     }),
     MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigService) => configService.get('mongoConfig'),
+      useFactory: (configService: ConfigService) => configService.get("mongoConfig"),
       inject: [ConfigService],
     }),
     UsersModule,
--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,18 +1,39 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule, getConnectionToken } from "@nestjs/mongoose";
+import { Test, TestingModule } from "@nestjs/testing";
+import { Connection } from "mongoose";
+import mongoConfig from "../../config/mongoConfig";
+import UserSchema from "./schema/user.schema";
+import { UsersService } from "./users.service";
 
-describe('UsersService', () => {
+describe("UsersService", () => {
   let service: UsersService;
+  let mongooseConnection: Connection;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: ".dev.env",
+          cache: true,
+          isGlobal: true,
+          load: [mongoConfig],
+        }),
+        MongooseModule.forRoot(process.env.DB_URI),
+        MongooseModule.forFeature([{ name: "User", schema: UserSchema }]),
+      ],
       providers: [UsersService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    mongooseConnection = module.get<Connection>(getConnectionToken());
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  afterAll(async () => {
+    await mongooseConnection.close();
   });
 });
